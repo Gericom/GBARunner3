Gericom/GBARunner3

commit 195383c1ec3f78a845b52cd037036ed39d548163

Added some support for longer range pc-relative rom reads, added manual jit patches for Kong - The Animated Series. Fixes #67

code/core/arm9/source/MemoryEmulator/MemoryLoad16.s
@@ -88,6 +88,10 @@ arm_func memu_load16Bios
     bx lr
 
 arm_func memu_load16Ewram
+    cmp r8, #0x02400000
+    addhs r9, r8, #(0x08000000 - 0x02200000)
+    bhs memu_load16RomHiContinue
+
     bic r9, r8, #0x00FC0000
     ldrh r9, [r9]
     tst r8, #1
@@ -172,6 +176,7 @@ arm_func memu_load16Oam
 arm_func memu_load16Rom
     ldr r11,= (sdc_romBlockToCacheBlock - (0x08000000 >> (SDC_BLOCK_SHIFT - 2)))
     bic r12, r8, #(3 << (SDC_BLOCK_SHIFT - 2))
+memu_load16RomContinue:
     ldr r11, [r11, r12, lsr #(SDC_BLOCK_SHIFT - 2)]
     mov r9, r8, lsl #(32 - SDC_BLOCK_SHIFT)
     cmp r11, #0
@@ -184,18 +189,16 @@ arm_func memu_load16Rom
     bx lr
 
 load16RomCacheMiss:
-    mov r10, r13
     ldr r11,= dtcmStackEnd
     // check if we already had a stack
-    sub r9, r11, r13
-    cmp r9, #1024
+    sub r10, r11, r13
+    cmp r10, #1024
+    mov r10, r13
     // if not begin at the end of the stack
     movhs sp, r11
     push {r0-r3,lr}
-    mov r0, r8
+    mov r0, r12
     bl sdc_loadRomBlockDirect
-    mov r9, r8, lsl #(32 - SDC_BLOCK_SHIFT)
-    mov r9, r9, lsr #(32 - SDC_BLOCK_SHIFT)
     ldrh r9, [r0, r9]
     pop {r0-r3,lr}
     mov r13, r10
@@ -205,19 +208,11 @@ load16RomCacheMiss:
     bx lr
 
 arm_func memu_load16RomHi
-    ldr r11,= sdc_romBlockToCacheBlock
     bic r9, r8, #0x0E000000
+memu_load16RomHiContinue:
+    ldr r11,= (sdc_romBlockToCacheBlock - (0x08000000 >> (SDC_BLOCK_SHIFT - 2)))
     bic r12, r9, #(3 << (SDC_BLOCK_SHIFT - 2))
-    ldr r11, [r11, r12, lsr #(SDC_BLOCK_SHIFT - 2)]
-    mov r9, r8, lsl #(32 - SDC_BLOCK_SHIFT)
-    cmp r11, #0
-        beq load16RomCacheMiss
-    mov r9, r9, lsr #(32 - SDC_BLOCK_SHIFT)
-    ldrh r9, [r11, r9]
-    tst r8, #1
-        bxeq lr
-    mov r9, r9, ror #8
-    bx lr
+    b memu_load16RomContinue
 
 arm_func memu_load16Sram
     ldr r10,= gSaveData

code/core/arm9/source/MemoryEmulator/MemoryLoad32.s
@@ -111,6 +111,10 @@ arm_func memu_load32Bios
     bx lr
 
 arm_func memu_load32Ewram
+    cmp r8, #0x02400000
+    addhs r9, r8, #(0x08000000 - 0x02200000)
+    bhs memu_load32RomHiContinue
+
     bic r9, r8, #0x00FC0000
     ldr r9, [r9]
     bx lr
@@ -181,6 +185,7 @@ arm_func memu_load32Oam
 arm_func memu_load32Rom
     ldr r11,= (sdc_romBlockToCacheBlock - (0x08000000 >> (SDC_BLOCK_SHIFT - 2)))
     bic r12, r8, #(3 << (SDC_BLOCK_SHIFT - 2))
+memu_load32RomContinue:
     ldr r11, [r11, r12, lsr #(SDC_BLOCK_SHIFT - 2)]
     mov r9, r8, lsl #(32 - SDC_BLOCK_SHIFT)
     cmp r11, #0
@@ -188,32 +193,27 @@ arm_func memu_load32Rom
     bxne lr
 
 load32RomCacheMiss:
-    mov r10, r13
     ldr r11,= dtcmStackEnd
     // check if we already had a stack
-    sub r9, r11, r13
-    cmp r9, #1024
+    sub r10, r11, r13
+    cmp r10, #1024
+    mov r10, r13
     // if not begin at the end of the stack
     movhs sp, r11
     push {r0-r3,lr}
-    mov r0, r8
+    mov r0, r12
     bl sdc_loadRomBlockDirect
-    mov r9, r8, lsl #(32 - SDC_BLOCK_SHIFT)
     ldr r9, [r0, r9, lsr #(32 - SDC_BLOCK_SHIFT)]
     pop {r0-r3,lr}
     mov r13, r10
     bx lr
 
 arm_func memu_load32RomHi
-    ldr r11,= sdc_romBlockToCacheBlock
     bic r9, r8, #0x0E000000
+memu_load32RomHiContinue:
+    ldr r11,= (sdc_romBlockToCacheBlock - (0x08000000 >> (SDC_BLOCK_SHIFT - 2)))
     bic r12, r9, #(3 << (SDC_BLOCK_SHIFT - 2))
-    ldr r11, [r11, r12, lsr #(SDC_BLOCK_SHIFT - 2)]
-    mov r9, r8, lsl #(32 - SDC_BLOCK_SHIFT)
-    cmp r11, #0
-    ldrne r9, [r11, r9, lsr #(32 - SDC_BLOCK_SHIFT)]
-    bxne lr
-    b load32RomCacheMiss
+    b memu_load32RomContinue
 
 arm_func memu_load32Sram
     ldr r10,= gSaveData

code/core/arm9/source/MemoryEmulator/MemoryLoad8.s
@@ -48,6 +48,10 @@ arm_func memu_load8Bios
     bx lr
 
 arm_func memu_load8Ewram
+    cmp r8, #0x02400000
+    addhs r9, r8, #(0x08000000 - 0x02200000)
+    bhs memu_load8RomHiContinue
+
     bic r9, r8, #0x00FC0000
     ldrb r9, [r9]
     bx lr
@@ -119,6 +123,7 @@ arm_func memu_load8Oam
 arm_func memu_load8Rom
     ldr r11,= (sdc_romBlockToCacheBlock - (0x08000000 >> (SDC_BLOCK_SHIFT - 2)))
     bic r12, r8, #(3 << (SDC_BLOCK_SHIFT - 2))
+memu_load8RomContinue:
     ldr r11, [r11, r12, lsr #(SDC_BLOCK_SHIFT - 2)]
     mov r9, r8, lsl #(32 - SDC_BLOCK_SHIFT)
     cmp r11, #0
@@ -126,32 +131,27 @@ arm_func memu_load8Rom
     bxne lr
 
 load8RomCacheMiss:
-    mov r10, r13
     ldr r11,= dtcmStackEnd
     // check if we already had a stack
-    sub r9, r11, r13
-    cmp r9, #1024
+    sub r10, r11, r13
+    cmp r10, #1024
+    mov r10, r13
     // if not begin at the end of the stack
     movhs sp, r11
     push {r0-r3,lr}
-    mov r0, r8
+    mov r0, r12
     bl sdc_loadRomBlockDirect
-    mov r9, r8, lsl #(32 - SDC_BLOCK_SHIFT)
     ldrb r9, [r0, r9, lsr #(32 - SDC_BLOCK_SHIFT)]
     pop {r0-r3,lr}
     mov r13, r10
     bx lr
 
 arm_func memu_load8RomHi
-    ldr r11,= sdc_romBlockToCacheBlock
     bic r9, r8, #0x0E000000
+memu_load8RomHiContinue:
+    ldr r11,= (sdc_romBlockToCacheBlock - (0x08000000 >> (SDC_BLOCK_SHIFT - 2)))
     bic r12, r9, #(3 << (SDC_BLOCK_SHIFT - 2))
-    ldr r11, [r11, r12, lsr #(SDC_BLOCK_SHIFT - 2)]
-    mov r9, r8, lsl #(32 - SDC_BLOCK_SHIFT)
-    cmp r11, #0
-    ldrneb r9, [r11, r9, lsr #(32 - SDC_BLOCK_SHIFT)]
-    bxne lr
-    b load8RomCacheMiss
+    b memu_load8RomContinue
 
 arm_func memu_load8Sram
     ldr r10,= gSaveData

commit 7b3e303fd17426637a93768e7ab7a2cecd7e9909

Fixed regression in commit 195383c, fixed /1024 timer mode causing freezes. Fixes #79

code/core/arm7/source/Sound/GbaTimer.c
@@ -4,7 +4,7 @@
 #define TICKS_PER_UPDATE    512
 #define TICK_SHIFT          1
 
-static const u32 sPrescaleShiftedTicksPerUpdate[3] =
+static const u32 sPrescaleShiftedTicksPerUpdate[4] =
 {
     TICKS_PER_UPDATE << TICK_SHIFT,
     (TICKS_PER_UPDATE << TICK_SHIFT) >> 6,

code/core/arm9/source/MemoryEmulator/MemoryLoad16.s
@@ -179,9 +179,9 @@ arm_func memu_load16Rom
 memu_load16RomContinue:
     ldr r11, [r11, r12, lsr #(SDC_BLOCK_SHIFT - 2)]
     mov r9, r8, lsl #(32 - SDC_BLOCK_SHIFT)
+    mov r9, r9, lsr #(32 - SDC_BLOCK_SHIFT)
     cmp r11, #0
         beq load16RomCacheMiss
-    mov r9, r9, lsr #(32 - SDC_BLOCK_SHIFT)
     ldrh r9, [r11, r9]
     tst r8, #1
         bxeq lr